cc647819a4f22f86bdd0bb577336342b5150a38a
onsjannet <onsjannet@gmail.com>
2021-02-14 21:42:42 +0100

    correcting

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -19,25 +19,15 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/1-hbnb/', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-
-    amenities = storage.all(Amenity).values()
-    amenities = sorted(amenities, key=lambda k: k.name)
-
-    places = storage.all(Place).values()
-    places = sorted(places, key=lambda k: k.name)
-
-    cache_id = str(uuid.uuid4())
+    states = storage.all('State')
+    amenities = storage.all('Amenity')
+    places = storage.all('Place')
+    cache_id = uuid.uuid4()
     return render_template('1-hbnb.html',
-                           states=st_ct,
+                           states=states,
                            amenities=amenities,
                            places=places,
                            cache_id=cache_id)
