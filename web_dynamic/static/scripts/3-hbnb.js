4a38ce250c80e484c5eb7e728ea42e254e56b7ab
onsjannet <onsjannet@gmail.com>
2021-02-15 19:55:06 +0100

    semistandard 3

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,12 +1,13 @@
+const $ = window.$;
+const checkedAmenities = {};
 $(document).ready(function () {
-  let checkedAmenities = {};
   $(document).on('change', "input[type='checkbox']", function () {
     if (this.checked) {
       checkedAmenities[$(this).data('id')] = $(this).data('name');
     } else {
       delete checkedAmenities[$(this).data('id')];
     }
-    let lst = Object.values(checkedAmenities);
+    const lst = Object.values(checkedAmenities);
     if (lst.length > 0) {
       $('div.amenities > h4').text(Object.values(checkedAmenities).join(', '));
     } else {
@@ -30,7 +31,7 @@ $(document).ready(function () {
     contentType: 'application/json',
     success: function (data) {
       for (let i = 0; i < data.length; i++) {
-        let place = data[i];
+        const place = data[i];
         $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
       }
     }
