f0e42d1c2c784897128a283bb1f13350782b4815
onsjannet <onsjannet@gmail.com>
2021-02-15 19:33:15 +0100

    task5

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -41,5 +41,11 @@ $(document).ready(function () {
         url: 'http://0.0.0.0:5001/api/v1/places_search/',
         type: 'POST',
         data: JSON.stringify({'amenities': Object.keys(checked)}),
+        success: function (data) {
+            for (let i = 0; i < data.length; i++) {
+              let place = data[i];
+              $('.places ').append('<article><h2>' + place.name + '</h2><div class="price_by_night"><p>$' + place.price_by_night + '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+            }
+          }
+        });
       });
-    });
