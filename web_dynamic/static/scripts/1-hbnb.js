488cbc7cf969fa489d4e0fb75c9e8b02c5046a29
onsjannet <onsjannet@gmail.com>
2021-02-15 20:42:26 +0100

    correction

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
-  let amenities_id = {};
-  $('input[type="checkbox"]').click(function(){
-      if($(this).prop("checked") == true){
-          amenities_id[$(this).data('id')] = $(this).data('name');
-      }
-      else if($(this).prop("checked") == false){
-          delete amenities_id[$(this).attr('data-id')];;
-      }
-      $('.amenities H4').text(Object.values(amenities).join(', '));
+const $ = window.$;
+$(document).ready(function () {
+  const amenitiesId = {};
+  $('input[type="checkbox"]').click(function () {
+    if ($(this).prop('checked') === true) {
+      amenitiesId[$(this).data('id')] = $(this).data('name');
+    } else if ($(this).prop('checked') === false) {
+      delete amenitiesId[$(this).attr('data-id')];
+    }
+    $('div.amenities H4').text(Object.values(amenitiesId).join(', '));
   });
 });
