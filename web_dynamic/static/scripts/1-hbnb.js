cc0758c1108862939fe864679c643f332255c7de
onsjannet <onsjannet@gmail.com>
2021-02-15 07:54:40 +0100

    JS

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    let amenities_id = {};
-    $('input[type="checkbox"]').click(function(){
-        if($(this).prop("checked") == true){
-            amenities_id[$(this).data('id')] = $(this).data('name');
-        }
-        else if($(this).prop("checked") == false){
-            delete amenities_id[$(this).attr('data-id')];;
-        }
-        $('.amenities H4').text(Object.values(amenities).join(', '));
-    });
+  let amenities_id = {};
+  $('input[type="checkbox"]').click(function(){
+      if($(this).prop("checked") == true){
+          amenities_id[$(this).data('id')] = $(this).data('name');
+      }
+      else if($(this).prop("checked") == false){
+          delete amenities_id[$(this).attr('data-id')];;
+      }
+      $('.amenities H4').text(Object.values(amenities).join(', '));
+  });
 });
